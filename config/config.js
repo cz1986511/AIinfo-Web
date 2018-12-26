1e843af8c28e684ea04feac75bde6212488ddedf
chenzhuo <chenzhuo@danlu.com>
2018-12-26 14:46:43 +0800

    add file

---
+++ config/config.js
@@ -0,0 +1,11 @@
+export default {
+    plugins: [
+        ['umi-plugin-react', {
+            //TODO
+        }],
+    ],
+    routes: [{
+        path: '/',
+        component: './HelloWorld',
+    }],
+};

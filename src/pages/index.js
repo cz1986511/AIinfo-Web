b7b14b7103b9ccfc64443c2816014b5fc7e42636
chenzhuo <chenzhuo@danlu.com>
2019-01-09 14:08:34 +0800

    add file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,61 @@
 import React, { Component } from 'react';
 import { Card } from 'antd';
-import { Tabs, Icon, BackTop, Alert } from 'antd';
+import { Tabs, BackTop, Alert } from 'antd';
+import { Button, Modal, Form, Input, Checkbox, Row, Col } from 'antd';
 import { connect } from 'dva';
 import myStyles from './styles.css';
 
 const namespace = 'indexlist';
+
+const CollectionCreateForm = Form.create({ name: 'form_in_modal' })(
+  class extends React.Component {
+    render() {
+      const {
+        visible, onCancel, onCreate, form,
+      } = this.props;
+      const { getFieldDecorator } = form;
+      const { TextArea } = Input;
+      return (
+        <Modal
+          visible={visible}
+          title="Create a new idea"
+          okText="Create"
+          onCancel={onCancel}
+          onOk={onCreate}
+        >
+          <Form layout="vertical">
+            <Form.Item label="Title">
+              {getFieldDecorator('title', {
+                rules: [{ required: true, message: 'Please input the title of collection!' }],
+              })(
+                <Input />
+              )}
+            </Form.Item>
+            <Form.Item label="Author">
+              {getFieldDecorator('author')(<Input />)}
+            </Form.Item>
+            <Form.Item label="Tag">
+              {getFieldDecorator('tag', {initialValue: ["B2B"],})
+              (
+                <Checkbox.Group style={{ width: "100%" }}>
+                  <Row>
+                    <Col span={5}><Checkbox value="B2B">B2B</Checkbox></Col>
+                    <Col span={5}><Checkbox value="研发">研发</Checkbox></Col>
+                    <Col span={5}><Checkbox value="架构">架构</Checkbox></Col>
+                    <Col span={5}><Checkbox value="创新">创新</Checkbox></Col>
+                  </Row>
+                </Checkbox.Group>
+              )}
+            </Form.Item>
+            <Form.Item label="Content" className="collection-create-form_last-form-item">
+              {getFieldDecorator('content')(<TextArea placeholder="Autosize" autosize={{ minRows: 2, maxRows: 6 }} />)}
+            </Form.Item>
+          </Form>
+        </Modal>
+      );
+    }
+  }
+);
 
 const mapStateToProps = (state) => {
   const artList = state[namespace].arts;
@@ -35,6 +86,35 @@ export default class ArtListPage extends Component {
     this.props.onDidMount();
   }
 
+  state = {
+    visible: false,
+  };
+
+  showModal = () => {
+    this.setState({ visible: true });
+  }
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  }
+
+  handleCreate = () => {
+    const form = this.formRef.props.form;
+    form.validateFields((err, values) => {
+      if (err) {
+        return;
+      }
+
+      console.log('Received values of form: ', values);
+      form.resetFields();
+      this.setState({ visible: false });
+    });
+  }
+
+  saveFormRef = (formRef) => {
+    this.formRef = formRef;
+  }
+
   render() {
     const oilinfo = this.props.oilinfo;
     const weather = this.props.weather;
@@ -43,7 +123,7 @@ export default class ArtListPage extends Component {
     const sport = suggestion.sport;
     const car_washing = suggestion.car_washing;
     const weatherinfos = oilinfo.updatetime + ' 成都-' + now.text + '-' + now.temperature + '°(运动:' + sport.brief + '|洗车:' + car_washing.brief + ')';
-    const oilinfos = '四川油价: 92#(' + oilinfo.oil93 + ')|95#(' + oilinfo.oil95 + ')|0#(' + oilinfo.oil0 + ')';
+    const oilinfos = '四川今日油价: 92#(' + oilinfo.oil93 + ')|95#(' + oilinfo.oil95 + ')|0#(' + oilinfo.oil0 + ')';
     return (
       <div>
         <div className={myStyles.header1}>
@@ -75,7 +155,17 @@ export default class ArtListPage extends Component {
               })
             }
             </TabPane>
-            <TabPane tab="敬请期待" key="2">页面开发中。。。</TabPane>
+            <TabPane tab="敬请期待" key="2">
+              <div>
+                <Button type="primary" onClick={this.showModal}>New</Button>
+                <CollectionCreateForm
+                  wrappedComponentRef={this.saveFormRef}
+                  visible={this.state.visible}
+                  onCancel={this.handleCancel}
+                  onCreate={this.handleCreate}
+                />
+              </div>
+            </TabPane>
           </Tabs>
         </div>
         <div>
